a6d7dcfdd4b01c06a46c19ae494f8fd6db28aa08
Ali Can Kaynarca <alicankaynarca@gmail.com>
2018-11-16 15:26:56 +0300

    create scenario

---
+++ scenario.js
@@ -0,0 +1,22 @@
+
+const friend = '905056466328'    // . this is a mobile number
+const home = '902322551164'      // this is a land line number
+const vox = '16502493791'      // this is a Voximplant purcahsed number 
+
+
+VoxEngine.addEventListener(AppEvents.CallAlerting, e => {
+  call = VoxEngine.callPSTN(home, vox)
+  // call = VoxEngine.callPSTN(home, e.callerid)
+  call.addEventListener(CallEvents.Ringing, () => {trace('ringing..')})
+  call.addEventListener(CallEvents.Connected, e2 => {
+    e.call.answer()
+    VoxEngine.sendMediaBetween(e.call, call)
+  })
+
+})
+
+
+
+
+
+
